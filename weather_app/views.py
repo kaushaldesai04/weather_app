00a59673ce895747d9758c2ed589ae20459f30c6
kaushal desai <69980544+kaushaldesai04@users.noreply.github.com>
2021-08-19 10:03:36 +0530

    Update views.py

--- weather_app/views.py
+++ weather_app/views.py
@@ -1,21 +1,29 @@
 import requests
+from datetime import date
 from django.shortcuts import render
 from .models import City
 from .forms import CityForm
 
 def index(request):
     url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid='
-
+    cities = City.objects.all()
+    ct=[]
+    for i in cities:
+        ct.append(i.name.upper())
+    print(ct)    
+    
     if request.method == 'POST':
         form = CityForm(request.POST)
-        form.save()
+        if form.data['name'].upper() not in ct:
+            form.save()
+        
 
     form = CityForm()
-
-    cities = City.objects.all()
+    today=date.today()
+    
     weather_data = []
     names=[]
-    print(cities)
+    
     for i in cities:
         if i.name.upper() not in names:
             names.append(i.name.upper())
@@ -29,11 +37,12 @@ def index(request):
                     'description' : r['weather'][0]['description'],
                     'icon' : r['weather'][0]['icon']
                 }
-            print(data)
+            
             weather_data.append(data)
         else:
             continue
-    print(names)
-    final={'weather_data':weather_data,'form':form}
+    
+    final={'weather_data':weather_data,'form':form,'date':today}
 
     return render(request, 'index.html',final)
+
